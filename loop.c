097f11f1fbca114b6c091105db8855b77b689b83
Seong-uk-An <asu970602@naver.com>
2021-09-06 15:09:11 +0900

    floor

--- loop.c
+++ loop.c
@@ -48,14 +48,83 @@ int main(void)
     //     printf("i는 %d\n", i);
     // }
 
-    // while
     // while(조건) {}
-    int i = 1;
-    while (i <= 10)
+    // int i = 1;
+    // while (i <= 10)
+    // {
+    //     printf("i는 %d\n", i++);
+    //     // i++
+    // }
+
+    // do {  } while (조건);
+    // int i = 1;
+    // do
+    // {
+    //     printf("i는 %d\n", i++);
+    // } while (i <= 10);
+
+    // 2중 반복문
+    // for (int i = 1; i <= 3; i++)
+    // {
+    //     printf("첫 번째 반복문 : %d\n", i);
+    //     for (int j = 1; j <= 5; j++)
+    //     {
+    //         printf("        두 번째 반복문 : %d\n", j);
+    //     }
+    // }
+
+    // 구구단
+    // 2 x 1 = 2
+    // 2 x 2 = 4
+    // 2 x 3 = 6
+    // 2 x 4 = 8
+    //
+    // 9 x 9 =81
+    // for (int i = 2; i <= 9; i++)
+    // {
+    //     printf(" %d단 게산\n", i);
+    //     for (int j = 1; j <= 9; j++)
+    //     {
+    //         printf("%d x %d = %d\n", i, j, i * j);
+    //     }
+    // }
+
+    /*
+    *
+    **
+    ***
+    ****
+    *****
+    */
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = 0; j <= i; j++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     for (int j = i; j < 4; j++)
+    //     {
+    //         printf(" ");
+    //     }
+    //     for (int k = 0; k <= i; k++)
+    //     {
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+
+    // 피라미드 쌓기
+    int floor;
+    prinf("몇층으로 쌓을 건가요?");
+    scanf("%d", &floor);
+    for (int i = 0; i < floor; i++)
     {
-        printf("i는 %d\n", i++);
-        // i++
-    }
+        }
 
     return 0;
 }
