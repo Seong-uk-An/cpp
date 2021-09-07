9a799a5293524601439b86887d7c2ee03f8dce95
Seong-uk-An <asu970602@naver.com>
2021-09-07 15:45:38 +0900

    string

---
+++ array.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // 배열
+    // int subway_array[3];
+    // subway_array[0] = 20;
+    // subway_array[1] = 30;
+    // subway_array[2] = 40;
+
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     printf("지하철%d호차에 %d명이 타고 있습니다.\n", i + 1, subway_array[i]);
+    // }
+
+    //값 설정 방법
+    // int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%d\n", arr[i]);
+    // }
+
+    // int arr[10];
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     printf("%d\n", arr[i]);
+    // }
+
+    // int arr[] = {1, 2};
+    // for (int i = 0; i < 2; i++)
+    // {
+    //     printf("%d\n", arr[i]);
+    // }
+
+    // float arr_f[5] = {1.0f, 2.0f, 3.0f};
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%.2f\n", arr_f[i]);
+    // }
+
+    char str[6] = "coding";
+    printf("%s\n", str);
+
+    return 0;
+}
